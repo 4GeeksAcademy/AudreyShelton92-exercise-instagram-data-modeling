4fac30be3b82fe15c5b1676e7b90e1365c782306
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-11-02 00:29:52 +0000

    completed

--- src/models.py
+++ src/models.py
@@ -7,31 +7,58 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(550), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    followers = relationship('Followers', back_populates='user')
+    post = relationship('Post', back_populates='user')
+
+class Followers(Base):
+    __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    
 
-    def to_dict(self):
-        return {}
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    
+
+    followers_id = Column(Integer, ForeignKey('followers.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    
+
+    followers_id = Column(Integer, ForeignKey('followers.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
